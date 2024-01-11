d8e2bfc0ac603deadd1d99dddf98c91587026703
GABRIEL STEINER <134329403+GabrielSteiner98@users.noreply.github.com>
2024-01-11 17:23:24 -0300

    Add files via upload

---
+++ Fornecedor.cs
@@ -0,0 +1,21 @@
+namespace LHPet.Models;
+
+public class Fornecedor
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    public string Cpf { get; set; }
+   
+    public string Email { get; set; }
+   
+
+ public Fornecedor(int id, string nome, string cpf, string email)
+    {
+        this.Id = Id;
+        this.Nome =nome;
+        this.Cpf =cpf;
+        this.Email = email;
+    }
+}
+
